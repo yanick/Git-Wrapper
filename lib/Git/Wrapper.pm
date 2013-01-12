ed961d3e70c73107161c7fa88cc00ce84e959b6d
John SJ Anderson <genehack@genehack.org>
2013-01-12 11:51:52 -0500

    Rework constructor to support better API (#32)
    Support the old undocumented crufty API in case somebody is using it,
    but also support a couple more sane ways.

--- lib/Git/Wrapper.pm
+++ lib/Git/Wrapper.pm
@@ -23,12 +23,27 @@ use Git::Wrapper::File::RawModification;
 use Git::Wrapper::Log;
 use Git::Wrapper::Statuses;
 
-my $GIT = ( defined $ENV{GIT_WRAPPER_GIT} ) ? $ENV{GIT_WRAPPER_GIT} : 'git';
-
 sub new {
-  my ($class, $arg, %opt) = @_;
+  my $class = shift;
 
-  my $self = bless { dir => $arg, %opt } => $class;
+  # three calling conventions
+  # 1: my $gw = Git::Wrapper->new( $dir )
+  # 2: my $gw = Git::Wrapper->new( $dir , %options )
+  # 3: my $gw = Git::Wrapper->new({ dir => $dir , %options });
+
+  my $args;
+
+  if ( scalar @_ == 1 ) {
+    my $arg = shift;
+    if ( ! ref $arg ) { $args = { dir => $arg } }
+    elsif ( ref $arg eq 'HASH' ) { $args = $arg }
+  }
+  else {
+    my( $dir , %opts ) = @_;
+    $args = { dir => $dir , %opts }
+  }
+
+  my $self = bless $args => $class;
 
   die "usage: $class->new(\$dir)" unless $self->dir;
 
@@ -38,6 +53,14 @@ sub new {
 sub has_git_in_path { can_run('git') }
 
 sub dir { shift->{dir} }
+
+sub git {
+  my $self = shift;
+
+  return $self->{git_binary} if defined $self->{git_binary};
+
+  return ( defined $ENV{GIT_WRAPPER_GIT} ) ? $ENV{GIT_WRAPPER_GIT} : 'git';
+}
 
 sub ERR { shift->{err} }
 sub OUT { shift->{out} }
@@ -61,7 +84,7 @@ sub RUN {
 
   my $opt = ref $_[0] eq 'HASH' ? shift : {};
 
-  my @cmd = $self->{'git_binary'} || $GIT;
+  my @cmd = $self->git;
 
   my $stdin = delete $opt->{-STDIN};
 
@@ -339,6 +362,12 @@ __END__
   $git->commit(...)
   print $_->message for $git->log;
 
+  # specify which git binary to use
+  my $git = Git::Wrapper->new({
+    dir        => '/var/foo ,
+    git_binary => '/path/to/git/bin/git' ,
+  });
+
 =head1 DESCRIPTION
 
 Git::Wrapper provides an API for git(7) that uses Perl data structures for
@@ -395,9 +424,14 @@ The exception stringifies to the error message.
 =head2 new
 
   my $git = Git::Wrapper->new($dir);
+  my $git = Git::Wrapper->new({ dir => $dir , git_binary => '/path/to/git' });
 
   # To force the git binary location
   my $git = Git::Wrapper->new($dir, 'git_binary' => '/usr/local/bin/git');
+
+=head2 git
+
+  print $git->git; # /path/to/git/binary/being/used
 
 =head2 dir
 
--- t/git_binary.t
+++ t/git_binary.t
@@ -3,14 +3,26 @@
 use strict;
 use warnings;
 use Test::More;
+use Test::Exception;
 
 use File::Temp qw(tempdir);
 use Git::Wrapper;
 
 my $dir = tempdir(CLEANUP => 1);
-my $git = Git::Wrapper->new($dir, 'git_binary' => 'echo' );
 
-my @got = $git->RUN('marco');
-is_deeply(\@got, ['marco'], "Wrapper runs what ever binary we tell it to");
+{
+  my $git = Git::Wrapper->new({ dir        => $dir ,
+                                git_binary => 'echo' });
+
+  my @got = $git->RUN('marco');
+  is_deeply(\@got, ['marco'], "Wrapper runs what ever binary we tell it to");
+}
+
+{
+  dies_ok { my $git = Git::Wrapper->new() } 'need a dir';
+  dies_ok { my $git = Git::Wrapper->new(['foo']) } 'need to call properly';
+  dies_ok { my $git = Git::Wrapper->new([dir => 'foo']) } 'no, really, need to call properly';
+  dies_ok { my $git = Git::Wrapper->new({ git_binary => "$dir/echo" }) } 'just git_binary fails';
+}
 
 done_testing();
